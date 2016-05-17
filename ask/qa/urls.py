4ea94d8009bb1f006ede751325dff45d6d951f7f
Viacheslav Alpatov <valp@inbox.ru>
2016-05-17 10:35:57 +0300

    no message

---
+++ ask/qa/urls.py
@@ -0,0 +1,8 @@
+urlpatterns = [
+    url(r'^question/(?P<pk>\d+)/', question_detail, name='question_detail'),
+    url(r'^popular/', popular, name='popular'),
+    url(r'^ask/', question_ask, name='question_ask'),
+    url(r'^signup/', user_signup, name='signup'),
+    url(r'^login/', user_login, name='login'),
+    url(r'^new/', test, name='new'),
+]
