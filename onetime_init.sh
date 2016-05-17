e55b8199e406c9dd4d91f263ba4f8338ed10b3ed
Viacheslav Alpatov <valp@inbox.ru>
2016-05-17 20:39:09 +0300

    no message

--- onetime_init.sh
+++ onetime_init.sh
@@ -2,3 +2,9 @@
 sudo pip install --upgrade django
 # Git
 git clone https://github.com/DonAlPatino/stepic.git /home/box/web
+
+# MySQL
+echo 'innodb_use_native_aio = 0' | sudo tee --append /etc/mysql/my.cnf
+sudo service mysql restart
+sudo mysql -uroot -e "CREATE DATABASE ask CHARACTER SET utf8 COLLATE utf8_general_ci;"
+sudo mysql -uroot -e "GRANT ALL PRIVILEGES ON ask.* TO 'ask_user'@'localhost' IDENTIFIED BY '123456789';"
