bc2cb1dc209a31ec016d2f4c222216312f2317a8
Viacheslav Alpatov <valp@inbox.ru>
2016-05-19 14:53:40 +0300

    no message

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -7,6 +7,28 @@ from qa.models import Question
 
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
+
+def paginate(request, qs):
+    try:
+        limit = int(request.GET.get('limit', 10))
+    except ValueError:
+        limit = 10
+    if limit > 100:
+        limit = 10
+
+    try:
+        page = int(request.GET.get('page', 1))
+    except ValueError:
+        raise Http404
+
+    paginator = Paginator(qs, limit)
+
+    try:
+        page = paginator.page(page)
+    except EmptyPage:
+        page = paginator.page(paginator.num_pages)
+    return page, paginator
+
 	
 def question_list(request):
     qs = Question.objects.all()
