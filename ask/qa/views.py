68c39dd95463b5df5c092b5ca9f44094bab9f982
Viacheslav Alpatov <valp@inbox.ru>
2016-05-20 14:40:16 +0300

    no message

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -3,10 +3,12 @@ from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.core.paginator import Paginator, EmptyPage
 from django.core.urlresolvers import reverse
 from qa.models import Question
+from qa.forms import AskForm, AnswerForm
 
 
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
+
 
 def paginate(request, qs):
     try:
@@ -29,7 +31,7 @@ def paginate(request, qs):
         page = paginator.page(paginator.num_pages)
     return page, paginator
 
-	
+
 def question_list(request):
     qs = Question.objects.all()
     qs = qs.order_by('-added_at')
@@ -41,7 +43,8 @@ def question_list(request):
         'page': page,
         'paginator': paginator,
     })
-	
+
+
 def popular(request):
     qs = Question.objects.all()
     qs = qs.order_by('-rating')
@@ -53,11 +56,41 @@ def popular(request):
         'page': page,
         'paginator': paginator,
     })
-	
+
+
 def question_detail(request, pk):
     question = get_object_or_404(Question, id=pk)
     answers = question.answer_set.all()
+    form = AnswerForm(initial={'question': str(pk)})
     return render(request, 'detail.html', {
         'question': question,
         'answers': answers,
+        'form': form,
     })
+
+
+def question_ask(request):
+    if request.method == 'POST':
+        form = AskForm(request.POST)
+        if form.is_valid():
+            form._user = request.user
+            ask = form.save()
+            url = reverse('question_detail', args=[ask.id])
+            return HttpResponseRedirect(url)
+    else:
+        form = AskForm()
+
+    return render(request, 'ask.html', {
+        'form': form
+    })
+
+
+def question_answer(request):
+    if request.method == 'POST':
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            form._user = request.user
+            answer = form.save()
+            url = reverse('question_detail', args=[answer.question.id])
+            return HttpResponseRedirect(url)
+    return HttpResponseRedirect('/')
